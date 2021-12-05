f7fa136525b8223522a6de67f13dcc956372fc2f
Brian nguyen <brian.ng142957@gmail.com>
2021-12-05 19:56:50 +0700

    Add

---
+++ sockets-golang/server/main_socket_server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// port := os.Getenv("PORT")
+	port := "5000"
+	service := fmt.Sprintf(":%s",port)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	fmt.Println("tcpAddr:", tcpAddr)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	fmt.Println("Socket run port:", service)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		// run as a goroutine
+		go handleClient(conn)
+
+		// handleClient(conn)
+        // conn.Close() // we're finished
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+
+	var buf [512]byte
+	fmt.Println("Message and RemoteAddr", string(buf[0:]), conn.RemoteAddr())
+	for {
+		// read upto 512 bytes
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+
+		fmt.Println("Message and RemoteAddr", string(buf[0:n]), conn.RemoteAddr())
+
+		daytime := time.Now().String()
+		// write the n bytes read
+		_, err2 := conn.Write([]byte(daytime))
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
